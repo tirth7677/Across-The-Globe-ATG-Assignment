6c7d29092a1c168b083331c4130dcc6b6cb06175
tirth7677 <tirthmalli6758@gmail.com>
2024-05-18 15:45:25 +0530

    data encryption

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const https = require("https");
+const fs = require("fs");
 const cors = require("cors");
 const connectDb = require("./Config/dbconnection.js");
 const dotenv = require("dotenv");
@@ -17,16 +19,29 @@ app.use(express.json());
 // Load environment variables from .env file
 dotenv.config();
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 // Route handlers
 app.use("/v1/api/auth", authRouter);
 app.use("/v1/api/post", postRouter);
 app.use("/v1/api/likeAndcomment", likeAndcommentRouter);
 
-// Start the server
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+// SSL/TLS certificate files
+const privateKey = fs.readFileSync("path/to/your/private-key.pem", "utf8");
+const certificate = fs.readFileSync("path/to/your/certificate.pem", "utf8");
+const ca = fs.readFileSync("path/to/your/ca.pem", "utf8");
+
+const credentials = {
+    key: privateKey,
+    cert: certificate,
+    ca: ca,
+};
+
+// Start the HTTPS server
+const httpsServer = https.createServer(credentials, app);
+
+httpsServer.listen(port, () => {
+    console.log(`HTTPS Server running on port ${port}`);
     // Connect to the database
     connectDb();
     console.log("Connected to the database");
